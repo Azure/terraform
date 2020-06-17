ba017e13724ea146e9a8f51b78056d9f9a3bbc6f
Julien Corioland <jucoriol@microsoft.com>
2020-06-17 12:00:16 +0200

    add basic structure for end-to-end go test

---
+++ samples/end-to-end-testing/src/test/end2end_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+)
+
+func TestEndToEndDeploymentScenario(t *testing.T) {
+	t.Parallel()
+
+	fixtureFolder := "../"
+
+	// User Terratest to deploy the infrastructure
+	test_structure.RunTestStage(t, "setup", func() {
+		terraformOptions := &terraform.Options{
+			// Indicate the directory that contains the Terraform configuration to deploy
+			TerraformDir: fixtureFolder,
+		}
+
+		// Save options for later test stages
+		test_structure.SaveTerraformOptions(t, fixtureFolder, terraformOptions)
+
+		// Triggers the terraform init and terraform apply command
+		terraform.InitAndApply(t, terraformOptions)
+	})
+
+	test_structure.RunTestStage(t, "validate", func() {
+		// run validation checks here
+	})
+
+	// When the test is completed, teardown the infrastructure by calling terraform destroy
+	test_structure.RunTestStage(t, "teardown", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, fixtureFolder)
+		terraform.Destroy(t, terraformOptions)
+	})
+}
---
+++ samples/end-to-end-testing/src/test/go.mod
@@ -0,0 +1,5 @@
+module test
+
+go 1.13
+
+require github.com/gruntwork-io/terratest v0.28.5
