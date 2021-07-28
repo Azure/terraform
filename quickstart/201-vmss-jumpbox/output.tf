8d4c15722d79ab721a9d3f68025386944fbcf377
Tom Archer <tom.archer@microsoft.com>
2021-07-28 14:48:27 -0700

    Changes to output

--- quickstart/201-vmss-jumpbox/output.tf
+++ quickstart/201-vmss-jumpbox/output.tf
@@ -1,7 +1,11 @@
-output "vmss_public_ip" {
-    value = azurerm_public_ip.vmss.fqdn
+output "vmss_public_ip_fqdn" {
+     value = azurerm_public_ip.vmss.fqdn
+}
+
+output "jumpbox_public_ip_fqdn" {
+   value = azurerm_public_ip.jumpbox.fqdn
 }
 
 output "jumpbox_public_ip" {
-    value = azurerm_public_ip.jumpbox.fqdn
+   value = azurerm_public_ip.jumpbox.ip_address
 }
