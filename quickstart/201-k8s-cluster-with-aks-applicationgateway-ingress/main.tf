da2472a399aa60212262230aef1d1d4b47fc7a83
Tom Archer <tom.archer@microsoft.com>
2021-08-01 19:42:50 -0700

    201-k8s-cluster-with-aks-applicationgateway-ingress

---
+++ quickstart/201-k8s-cluster-with-aks-applicationgateway-ingress/main.tf
@@ -0,0 +1,18 @@
+terraform {
+required_providers {
+  azurerm = {
+    source = "hashicorp/azurerm"
+    version = "~>2.0"
+  }
+}
+backend "azurerm" {
+  resource_group_name = var.resource_group_name
+  storage_account_name = var.storage_account_name
+  container_name = "tfstate"
+  key = "codelab.microsoft.tfstate"
+}
+}
+
+provider "azurerm" {
+features {}
+}
