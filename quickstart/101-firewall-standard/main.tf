eeb9d1e303ba50b556507b8097e1ff242ff133fa
Vic Horne <victorh@microsoft.com>
2021-08-05 12:05:11 -0700

    add required_providers

--- quickstart/101-firewall-standard/main.tf
+++ quickstart/101-firewall-standard/main.tf
@@ -1,5 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.46.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = "=1.36.0"
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
