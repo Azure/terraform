bd3972caabf1d93d28aa990cbfb225b7b6b102b9
Tom Archer <tom.archer@microsoft.com>
2021-08-06 16:29:03 -0700

    Added min tf version

--- quickstart/201-k8s-cluster-with-aks-applicationgateway-ingress/main.tf
+++ quickstart/201-k8s-cluster-with-aks-applicationgateway-ingress/main.tf
@@ -1,18 +1,21 @@
 terraform {
-required_providers {
-  azurerm = {
-    source = "hashicorp/azurerm"
-    version = "~>2.0"
-  }
-}
-backend "azurerm" {
-  resource_group_name = var.resource_group_name
-  storage_account_name = var.storage_account_name
-  container_name = "tfstate"
-  key = "codelab.microsoft.tfstate"
-}
-}
 
-provider "azurerm" {
-features {}
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name = "tfstate"
+    key = "codelab.microsoft.tfstate"
+  }
+  }
+
+  provider "azurerm" {
+  features {}
 }
--- quickstart/201-vmss-jumpbox/main.tf
+++ quickstart/201-vmss-jumpbox/main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">=0.12"
+  
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
