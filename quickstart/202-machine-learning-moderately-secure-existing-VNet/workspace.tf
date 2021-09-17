00396b82ebb6a706e229ec8ef2218df55b9e3a81
Dennis Eikelenboom <mail@denniseik.nl>
2021-09-17 15:25:11 -0700

    Update compute timeout

--- quickstart/202-machine-learning-moderately-secure-existing-VNet/workspace.tf
+++ quickstart/202-machine-learning-moderately-secure-existing-VNet/workspace.tf
@@ -173,7 +173,7 @@ resource "azurerm_machine_learning_compute_cluster" "image-builder" {
   scale_settings {
     min_node_count                       = 0
     max_node_count                       = 1
-    scale_down_nodes_after_idle_duration = "PT30S" # 30 seconds
+    scale_down_nodes_after_idle_duration = "PT15M" # 15 minutes
   }
 
   identity {
