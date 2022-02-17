226c83b637aeaa5a68cd60ab620ab571396540a3
neil-yechenwei <yechenwei2007@126.com>
2022-02-17 13:24:33 +0800

    update code

--- quickstart/201-postgresql-fs-db/output.tf
+++ quickstart/201-postgresql-fs-db/output.tf
@@ -1,3 +1,7 @@
+output "resource_group_name" {
+    value = azurerm_resource_group.default.name
+}
+
 output "postgresql_flexible_server_database_name" {
     value = azurerm_postgresql_flexible_server_database.default.name
 }
