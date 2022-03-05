744613923aa54e53d67ec1f1bca4344118ba1e6d
Tom Archer <tom.archer@microsoft.com>
2022-03-05 14:16:56 -0800

    Formatted code via 'terraform fmt'

--- quickstart/201-k8s-cluster-with-aks-applicationgateway-ingress/providers.tf
+++ quickstart/201-k8s-cluster-with-aks-applicationgateway-ingress/providers.tf
@@ -4,18 +4,18 @@ terraform {
 
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
   }
   backend "azurerm" {
-    resource_group_name = "<storage_account_resource_group>"
+    resource_group_name  = "<storage_account_resource_group>"
     storage_account_name = "<storage_account_name>"
-    container_name = "tfstate"
-    key = "codelab.microsoft.tfstate"
+    container_name       = "tfstate"
+    key                  = "codelab.microsoft.tfstate"
   }
-  }
+}
 
-  provider "azurerm" {
+provider "azurerm" {
   features {}
 }
