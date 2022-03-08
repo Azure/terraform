1ba0df3660447f1d45e4d49746b4b667e9d1b350
Vic Horne <victorh@microsoft.com>
2022-03-08 08:26:25 -0800

    create providers.tf

--- quickstart/101-application-gateway/main.tf
+++ quickstart/101-application-gateway/main.tf
@@ -1,19 +1,3 @@
-terraform {
-
-  required_version = ">=0.12"
-  
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=2.97.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg1" {
   name     = "myResourceGroupAG"
   location = "eastus"
