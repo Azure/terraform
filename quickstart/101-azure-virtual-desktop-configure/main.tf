14eb6932ae095df8c8e12d25c459ff78bd170b25
Tom Archer <tom.archer@microsoft.com>
2022-03-19 09:19:08 -0700

    Corrected expiration_date value

--- quickstart/101-azure-virtual-desktop-configure/main.tf
+++ quickstart/101-azure-virtual-desktop-configure/main.tf
@@ -41,7 +41,7 @@ resource "time_rotating" "avd_token" {
 
 resource "azurerm_virtual_desktop_host_pool_registration_info" "registrationinfo" {
   hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
-  expiration_date = time_rotating.avd_token.rfc3339
+  expiration_date = time_rotating.avd_token.rotation_rfc3339
 }
 
 # Create AVD DAG
