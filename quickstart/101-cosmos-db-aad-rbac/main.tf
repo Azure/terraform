466299115eb126e6b13b1daa51900ce7dbd310f1
Gin <ginc11232@gmail.com>
2022-06-30 14:28:17 +0000

    Separated providers into providers.tf

--- quickstart/101-cosmos-db-aad-rbac/main.tf
+++ quickstart/101-cosmos-db-aad-rbac/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.8.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "example" {
--- quickstart/101-cosmos-db-analyticalstore/main.tf
+++ quickstart/101-cosmos-db-analyticalstore/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.8.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
