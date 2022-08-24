26a919e903b8fd32a489086b603f6eb5a24def3c
Tom Archer <tarcher@microsoft.com>
2022-08-23 19:02:18 -0700

    Updating code to align with Contributor Guide template

--- quickstart/101-attestation-provider/main.tf
+++ quickstart/101-attestation-provider/main.tf
@@ -1,22 +1,10 @@
-terraform {
-
-  required_version = ">=0.12"
-    
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
+resource "random_pet" "rg-name" {
+  prefix    = var.resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
-  location = var.resource_group_location
+  name      = random_pet.rg-name.id
+  location  = var.resource_group_location
 }
 
 resource "azurerm_attestation_provider" "corpAttestation" {
