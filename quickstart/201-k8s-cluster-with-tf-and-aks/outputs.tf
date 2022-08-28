df14b6229d0675ccb48f898196a720a12083a3a0
Tom Archer <tarcher@microsoft.com>
2022-08-27 22:19:04 -0700

    Marked various output values as sensitive

--- quickstart/201-k8s-cluster-with-tf-and-aks/outputs.tf
+++ quickstart/201-k8s-cluster-with-tf-and-aks/outputs.tf
@@ -3,23 +3,28 @@ output "resource_group_name" {
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
 
 output "cluster_username" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.username
+  sensitive = true
 }
 
 output "cluster_password" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.password
+  sensitive = true
 }
 
 output "kube_config" {
@@ -28,6 +33,7 @@ output "kube_config" {
 }
 
 output "host" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+  sensitive = true
 }
     
