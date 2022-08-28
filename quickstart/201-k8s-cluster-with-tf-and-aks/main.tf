0419cad12a3b0eff0a83a5de2d1b388d06f8c383
Tom Archer <tarcher@microsoft.com>
2022-08-27 21:25:20 -0700

    Updating main to use new rg var name

--- quickstart/201-k8s-cluster-with-tf-and-aks/main.tf
+++ quickstart/201-k8s-cluster-with-tf-and-aks/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_log_analytics_workspace" "test" {
   # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
   name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
   location            = var.log_analytics_workspace_location
-  resource_group_name = azurerm_resource_group.k8s.name
+  resource_group_name = azurerm_resource_group.rg.name
   sku                 = var.log_analytics_workspace_sku
 }
 
