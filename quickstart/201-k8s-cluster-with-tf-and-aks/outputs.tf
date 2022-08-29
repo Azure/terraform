7305ee2a26e167c26c84ca98bb0574dc000ec6ac
Tom Archer <tarcher@microsoft.com>
2022-08-28 23:45:08 -0700

    changing array syntax

--- quickstart/201-k8s-cluster-with-tf-and-aks/outputs.tf
+++ quickstart/201-k8s-cluster-with-tf-and-aks/outputs.tf
@@ -33,7 +33,7 @@ output "kube_config" {
 }
 
 output "host" {
-  value     = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+  value     = azurerm_kubernetes_cluster.k8s.kube_config[0].host
   sensitive = true
 }
     
