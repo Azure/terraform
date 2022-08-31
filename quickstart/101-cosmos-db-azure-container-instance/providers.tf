2633db1ff1081bd2819f4353fac182b6ff202a38
Tom Archer <tarcher@microsoft.com>
2022-08-30 20:26:35 -0700

    Updated azurerm provider version constraint

--- quickstart/101-cosmos-db-azure-container-instance/providers.tf
+++ quickstart/101-cosmos-db-azure-container-instance/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>3.0"
     }
     random = {
       source  = "hashicorp/random"
