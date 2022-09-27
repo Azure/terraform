6c0ef0ac41202eaa664356844f716eacd18a80aa
xiaxin18 <xiaxyi@microsoft.com>
2022-09-27 16:50:18 +0800

    update

--- quickstart/201-function-app-key-vault-ref/main.tf
+++ quickstart/201-function-app-key-vault-ref/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_user_assigned_identity" "default" {
 }
 
 resource "azurerm_storage_account" "default" {
-  name                     = "${var.name_prefix}-sa"
+  name                     = "${var.name_prefix}sa"
   resource_group_name      = azurerm_resource_group.default.name
   location                 = azurerm_resource_group.default.location
   account_tier             = "Standard"
