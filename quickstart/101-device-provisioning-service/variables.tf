f4775c5bf65fad1ef92181123e117a53d73bc054
Kelly Gremban <kgremban@microsoft.com>
2022-09-29 14:52:07 -0700

    new quickstart sample 101-device-provisioning-service

---
+++ quickstart/101-device-provisioning-service/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name_prefix" {
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "storage_account_name_prefix" {
+  default     = "sa"
+  description = "Prefix of the storage account name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "eventhub_namespace_name_prefix" {
+  default     = "namespace"
+  description = "Prefix of the event hub namespace name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "iothub_name_prefix" {
+  default     = "iothub"
+  description = "Prefix of the iot hub name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "dps_name_prefix" {
+  default     = "dps"
+  description = "Prefix of the dps name that's combined with a random ID so name is unique in your Azure subscription."
+}
