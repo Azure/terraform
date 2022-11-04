e70d96752437234129f2dcab89325ec1e6be3683
Uros Randelovic <uros.randelovic@gmail.com>
2022-11-04 09:59:03 +0100

    remove depends on clauses

--- quickstart/101-managed-instance/main.tf
+++ quickstart/101-managed-instance/main.tf
@@ -55,9 +55,6 @@ resource "azurerm_route_table" "example" {
   location                      = azurerm_resource_group.example.location
   resource_group_name           = azurerm_resource_group.example.name
   disable_bgp_route_propagation = false
-  depends_on = [
-    azurerm_subnet.example,
-  ]
 }
 
 # Associate subnet and the route table
@@ -78,9 +75,4 @@ resource "azurerm_mssql_managed_instance" "example" {
   sku_name                     = var.sku_name
   vcores                       = var.vcores
   storage_size_in_gb           = var.storage_size_in_gb
-
-  depends_on = [
-    azurerm_subnet_network_security_group_association.example,
-    azurerm_subnet_route_table_association.example,
-  ]
 }
