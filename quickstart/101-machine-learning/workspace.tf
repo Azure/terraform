fb2d3928a9659eaf06d1a94e35bb59febd45c948
Joshua Loeffler <jloeffler@microsoft.com>
2022-11-17 11:03:43 -0500

    Explicitly disable storage blob public access for workspace templates

--- quickstart/101-machine-learning/workspace.tf
+++ quickstart/101-machine-learning/workspace.tf
@@ -21,6 +21,7 @@ resource "azurerm_storage_account" "default" {
   resource_group_name      = azurerm_resource_group.default.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  allow_nested_items_to_be_public = false
 }
 
 resource "azurerm_container_registry" "default" {
--- quickstart/202-machine-learning-moderately-secure-existing-VNet/workspace.tf
+++ quickstart/202-machine-learning-moderately-secure-existing-VNet/workspace.tf
@@ -26,6 +26,7 @@ resource "azurerm_storage_account" "default" {
   resource_group_name      = azurerm_resource_group.default.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  allow_nested_items_to_be_public = false
 
   network_rules {
     default_action = "Deny"
