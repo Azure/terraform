a522c444629ec01c658615f9be801dc6d7fc95f7
Dingjia Chen <48020536+Pumpkin-3906@users.noreply.github.com>
2023-02-09 21:06:33 -0600

    101-cosmos-db-analyticalstore patch (#143)
    * change azurerm version
    
    * add default values

--- quickstart/101-cosmos-db-analyticalstore/variables.tf
+++ quickstart/101-cosmos-db-analyticalstore/variables.tf
@@ -1,30 +1,36 @@
-variable "resource_group_name" {
+variable "name_prefix" {
   type        = string
-  description = "Resource group name"
+  default     = "101-cosmos-db-analyticalstore"
+  description = "Prefix for resource group name"
 }
 
 variable "location" {
   type        = string
+  default     = "westus"
   description = "Resource group location"
 }
 
 variable "cosmosdb_account_name" {
   type        = string
+  default     = "default-account-name"
   description = "Cosmos db account name"
 }
 
 variable "cosmosdb_account_location" {
   type        = string
+  default     = "westus"
   description = "Cosmos db account location"
 }
 
 variable "cosmosdb_sqldb_name" {
   type        = string
+  default     = "default-sqldb-name"
   description = "value"
 }
 
 variable "throughput" {
   type        = number
+  default     = 400
   description = "Cosmos db database throughput"
   validation {
     condition     = var.throughput >= 400 && var.throughput <= 1000000
@@ -38,10 +44,12 @@ variable "throughput" {
 
 variable "sql_container_name" {
   type        = string
+  default     = "default-sql-container-name"
   description = "SQL API container name."
 }
 
 variable "analytical_storage_ttl" {
   type        = number
+  default     = 0
   description = "Analytical Storage TTL in seconds."
 }
