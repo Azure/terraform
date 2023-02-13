292a5e8be4c4678d307ca57c46ea95845fa824d1
zjhe <hezijie@microsoft.com>
2023-02-13 11:33:22 +0800

    fix 201-aks-rbac-dashboard-admin

---
+++ quickstart/201-aks-rbac-dashboard-admin/versions.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">=1.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azuread" {}
+
+provider "random" {}
+
+# Define Kubernetes provider to use the AKS cluster
+provider "kubernetes" {
+  host = azurerm_kubernetes_cluster.default.kube_config[0].host
+
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+}
