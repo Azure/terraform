cab55217b48755e3d020e833ae597d30fd89f607
JT <100274846+jiaweitao001@users.noreply.github.com>
2023-02-13 17:47:34 +0800

    201-aks-log-analytics patch (#150)
    * 201-aks-log-analytics patch

---
+++ quickstart/201-aks-log-analytics/versions.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.2"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "random" {}
