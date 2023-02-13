dc7947832d2e3efcb43467c8f7239eeb6cb46bf6
zjhe <hezijie@microsoft.com>
2023-02-13 12:32:42 +0800

    fix 101-application-gateway

--- quickstart/101-application-gateway/providers.tf
+++ quickstart/101-application-gateway/providers.tf
@@ -1,11 +1,14 @@
 terraform {
-
-  required_version = ">=0.12"
+  required_version = ">=1.2"
   
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.97.0"
+      version = "~> 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
