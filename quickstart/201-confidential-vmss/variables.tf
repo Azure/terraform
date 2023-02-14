77c841daa2ce2dae49809c01a4a9d4519a8c2b13
Dingjia Chen <48020536+Pumpkin-3906@users.noreply.github.com>
2023-02-13 23:37:44 -0600

    201-confidential-vmss patch (#142)
    * fix 201-confidential-vmss example

--- quickstart/201-confidential-vmss/variables.tf
+++ quickstart/201-confidential-vmss/variables.tf
@@ -1,15 +1,11 @@
-variable "admin_password" {
-  type        = string
-  sensitive   = true
-  description = "Admin password of the virtual machine scale set"
-}
-
 variable "location" {
   type        = string
+  default     = "westus"
   description = "Location where resources will be created"
 }
 
 variable "name_prefix" {
   type        = string
+  default     = "201-confidential-vmss"
   description = "Prefix of the resource name"
 }
