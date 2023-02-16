a6dbd1ecb533ad5b2fee48d96a2e13860a342d1f
Pumpkin-3906 <anna88x88@gmail.com>
2023-02-16 00:20:23 -0600

    fmt

--- quickstart/201-vm-disk-encryption-extension/providers.tf
+++ quickstart/201-vm-disk-encryption-extension/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">=1.0"
+  required_version = ">= 1.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.0, < 4.0"
+      version = ">= 3.0, < 4.0"
     }
     local = {
       source  = "hashicorp/local"
