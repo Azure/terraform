a0db8af3a5874bd5be600d8a44b0d2923dc85cb4
Dingjia Chen <48020536+Pumpkin-3906@users.noreply.github.com>
2023-02-20 20:17:36 -0600

    fix example (#167)

--- quickstart/101-windows-vm-with-iis-server/outputs.tf
+++ quickstart/101-windows-vm-with-iis-server/outputs.tf
@@ -3,5 +3,10 @@ output "resource_group_name" {
 }
 
 output "public_ip_address" {
-  value = azurerm_windows_virtual_machine.my_terraform_vm.public_ip_address
+  value = azurerm_windows_virtual_machine.main.public_ip_address
+}
+
+output "admin_password" {
+  sensitive = true
+  value     = azurerm_windows_virtual_machine.main.admin_password
 }
