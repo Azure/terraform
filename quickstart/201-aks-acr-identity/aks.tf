a198987b19b6999239cb6d994dd988ee386bd6d5
hezijie <lonegunmanb@hotmail.com>
2023-02-21 10:38:21 +0800

    use identity

--- quickstart/201-aks-acr-identity/aks.tf
+++ quickstart/201-aks-acr-identity/aks.tf
@@ -1,24 +1,19 @@
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "${var.name}-aks"
-  location            = azurerm_resource_group.default.location
-  resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "${var.dns_prefix}-${var.name}-aks-${var.environment}"
-  depends_on          = ["azurerm_role_assignment.aks_network", "azurerm_role_assignment.aks_acr"]
+  name                              = "${var.name}-aks"
+  location                          = azurerm_resource_group.default.location
+  resource_group_name               = azurerm_resource_group.default.name
+  dns_prefix                        = "${var.dns_prefix}-${var.name}-aks-${var.environment}"
+  depends_on                        = ["azure_role_assignment.aks_network", "azurerm_role_assignment.aks_acr"]
+  role_based_access_control_enabled = true
 
-  agent_pool_profile {
+  default_node_pool {
     name            = "default"
-    count           = "${var.node_count}"
-    vm_size         = "${var.node_type}"
-    os_type         = "Linux"
+    vm_size         = var.node_type
+    node_count      = var.node_count
     os_disk_size_gb = 30
   }
-
-  service_principal {
-    client_id     = "${azuread_application.default.application_id}"
-    client_secret = "${azuread_service_principal_password.default.value}"
-  }
-
-  role_based_access_control {
-    enabled = true
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks.id]
   }
 }
--- quickstart/201-aks-acr-identity/main.tf
+++ quickstart/201-aks-acr-identity/main.tf
@@ -3,11 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "1.36.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "0.6.0"
+      version = "~> 3.0"
     }
   }
 }
