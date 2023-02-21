559ba2cdf0b78793817dd90c29101df70f95f1b3
hezijie <lonegunmanb@hotmail.com>
2023-02-21 11:27:41 +0800

    amending azurerm provider block to fix this exmaple

--- quickstart/201-aks-acr-identity/main.tf
+++ quickstart/201-aks-acr-identity/main.tf
@@ -8,6 +8,10 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 # Reference to the current subscription.  Used when creating role assignments
 data "azurerm_subscription" "current" {}
 
