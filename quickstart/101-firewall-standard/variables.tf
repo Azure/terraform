4e0ad3a89c1265e5ef03b32ec040d69bcd4ad0d0
Dingjia Chen <48020536+Pumpkin-3906@users.noreply.github.com>
2023-02-21 00:17:48 -0600

    101-firewall-standard patch (#169)
    * fix example
    
    * add variable type

--- quickstart/101-firewall-standard/variables.tf
+++ quickstart/101-firewall-standard/variables.tf
@@ -1,3 +1,11 @@
 variable "resource_group_location" {
-  default = "eastus"
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "firewall-standard"
+  description = "Prefix of the resource name"
 }
