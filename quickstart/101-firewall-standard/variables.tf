b4f1a1720210119bec7162923af7a273a452214c
Pumpkin-3906 <anna88x88@gmail.com>
2023-02-20 20:49:38 -0600

    fix example

--- quickstart/101-firewall-standard/variables.tf
+++ quickstart/101-firewall-standard/variables.tf
@@ -1,3 +1,10 @@
 variable "resource_group_location" {
-  default = "eastus"
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "firewall-standard"
+  description = "Prefix of the resource name"
 }
