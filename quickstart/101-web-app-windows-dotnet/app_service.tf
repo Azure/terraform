e70a9bc099c081f78b027ba107a00b27b50131cc
Pumpkin-3906 <anna88x88@gmail.com>
2023-02-23 20:53:46 -0600

    fix 101-web-app-windows-dotnet example

--- quickstart/101-web-app-windows-dotnet/app_service.tf
+++ quickstart/101-web-app-windows-dotnet/app_service.tf
@@ -1,20 +1,20 @@
 resource "azurerm_app_service_plan" "default" {
-  name                = "${var.name}-plan"
-  location            = "${azurerm_resource_group.default.location}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
+  name                = "${random_pet.prefix.id}-plan"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
   kind                = "Windows"
 
   sku {
-    tier = "${var.plan_tier}"
-    size = "${var.plan_sku}"
+    tier = var.plan_tier
+    size = var.plan_sku
   }
 }
 
 resource "azurerm_app_service" "default" {
-  name                = "${var.dns_prefix}-${var.name}-aspnet-${var.environment}-app"
-  location            = "${azurerm_resource_group.default.location}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.default.id}"
+  name                = "${var.dns_prefix}-${random_pet.prefix.id}-aspnet-${var.environment}-app"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  app_service_plan_id = azurerm_app_service_plan.default.id
 
   site_config {
     always_on                = true
