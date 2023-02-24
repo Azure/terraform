b2d0dbc2e73736fb515ca9945517a60897fabaa1
Dingjia Chen <48020536+Pumpkin-3906@users.noreply.github.com>
2023-02-24 01:23:15 -0600

    fix example (#176)

--- quickstart/201-web-app-docker-acr/main.tf
+++ quickstart/201-web-app-docker-acr/main.tf
@@ -1,11 +1,12 @@
-provider "azurerm" {
-  version = "=1.36.1"
-}
-
 # Reference to the current subscription.  Used when creating role assignments
 data "azurerm_subscription" "current" {}
 
 resource "azurerm_resource_group" "default" {
-  name     = "${var.name}-${var.environment}-rg"
-  location = "${var.location}"
+  name     = "${random_pet.prefix.id}-${var.environment}-rg"
+  location = var.location
+}
+
+resource "random_pet" "prefix" {
+  prefix = var.prefix
+  length = 2
 }
