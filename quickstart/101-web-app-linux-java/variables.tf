65c0e830c04b8e15a67ab7b35e5b92708cd9595b
Pumpkin-3906 <anna88x88@gmail.com>
2023-02-23 21:07:41 -0600

    fix 101-web-app-linux-java example

--- quickstart/101-web-app-linux-java/variables.tf
+++ quickstart/101-web-app-linux-java/variables.tf
@@ -1,35 +1,35 @@
-variable "name" {
-  type        = "string"
-  description = "Location of the azure resource group."
-  default     = "demo-tfquickstart"
+variable "prefix" {
+  type        = string
+  default     = "web-app-linux-java"
+  description = "Prefix of the resource name"
 }
 
 variable "environment" {
-  type        = "string"
+  type        = string
   description = "Name of the deployment environment"
   default     = "dev"
 }
 
 variable "location" {
-  type        = "string"
+  type        = string
   description = "Location to deploy the resource group"
-  default     = "West US 2"
+  default     = "eastus"
 }
 
 variable "dns_prefix" {
-  type        = "string"
+  type        = string
   description = "A prefix for any dns based resources"
   default     = "tfq"
 }
 
 variable "plan_tier" {
-  type        = "string"
+  type        = string
   description = "The tier of app service plan to create"
   default     = "Standard"
 }
 
 variable "plan_sku" {
-  type        = "string"
+  type        = string
   description = "The sku of app service plan to create"
   default     = "S1"
 }
