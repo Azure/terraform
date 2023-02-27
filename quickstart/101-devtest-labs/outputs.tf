33a101cd1d2284237dd7364b1329c1648f414a03
Tom Archer <tarcher@microsoft.com>
2023-02-27 12:14:54 -0800

    Added output of values

--- quickstart/101-devtest-labs/outputs.tf
+++ quickstart/101-devtest-labs/outputs.tf
@@ -1,5 +1,13 @@
-output "lab_id" {
-  value = azurerm_dev_test_lab.lab.id
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "lab_name" {
+  value = azurerm_dev_test_lab.lab.name
+}
+
+output "vm_name" {
+  value = azurerm_dev_test_windows_virtual_machine.vm.name
 }
 
 output "password" {
