5a1bf92dd1c8ebc7ca38ef6ace90d363f4ce891b
Tom Archer <tarcher@microsoft.com>
2023-03-07 05:46:49 -0800

    Ran 'terraform fmt'

--- quickstart/101-analysis-services-create/main.tf
+++ quickstart/101-analysis-services-create/main.tf
@@ -9,21 +9,21 @@ resource "azurerm_resource_group" "rg" {
 
 resource "random_string" "azurerm_analysis_services_server_name" {
   length  = 25
-  upper   = false 
+  upper   = false
   numeric = false
   special = false
 }
 
 resource "azurerm_analysis_services_server" "server" {
-  name                = random_string.azurerm_analysis_services_server_name.result
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = var.sku
+  name                      = random_string.azurerm_analysis_services_server_name.result
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  sku                       = var.sku
   backup_blob_container_uri = var.backup_blob_container_uri
 
   ipv4_firewall_rule {
-    name = "AllowFromAll"
+    name        = "AllowFromAll"
     range_start = "0.0.0.0"
-    range_end = "255.255.255.255"
+    range_end   = "255.255.255.255"
   }
 }
