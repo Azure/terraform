87a654c36ce2af05168ae353de51be2c6efefca6
Tom Archer <tarcher@microsoft.com>
2023-03-14 13:41:19 -0700

    Changing ver info per Zijie

--- quickstart/101-aci-linuxcontainer-public-ip/providers.tf
+++ quickstart/101-aci-linuxcontainer-public-ip/providers.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.4"
+      version = "~>3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>3.4"
+      version = "~>3.0"
     }
   }
 }
