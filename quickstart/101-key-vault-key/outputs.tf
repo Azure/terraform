335a26a9cf2941ba7b6c260ef489468ba1f08777
Tom Archer <tarcher@microsoft.com>
2023-03-27 17:41:09 -0700

    Changed outputs

--- quickstart/101-key-vault-key/outputs.tf
+++ quickstart/101-key-vault-key/outputs.tf
@@ -1,3 +1,11 @@
-output "proxy_key" {
-  value = azurerm_key_vault_key.key
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "azurerm_key_vault_name" {
+  value = azurerm_key_vault.vault.name
+}
+
+output "azurerm_key_vault_id" {
+  value = azurerm_key_vault.vault.id
 }
