d1bae87b2aea07dd722c3b4ab5e9f58ff658b7d1
Tom Archer <tarcher@microsoft.com>
2023-03-30 09:29:03 -0700

    Changes per Code Review

--- quickstart/101-batch-account-with-storage/variables.tf
+++ quickstart/101-batch-account-with-storage/variables.tf
@@ -15,7 +15,7 @@ variable "storage_account_type" {
   default     = "Standard_LRS"
   description = "Azure Storage account type."
   validation {
-    condition     = can(split("_", var.storage_account_type)[1])
-    error_message = "Storage account type must be a valid value, such as 'Standard_LRS'."
+    condition     = contains(["Premium_LRS", "Premium_ZRS", "Standard_GRS", "Standard_GZRS", "Standard_LRS", "Standard_RAGRS", "Standard_RAGZRS", "Standard_ZRS"], var.storage_account_type)
+    error_message = "Invalid storage account type. The value should be one of the following: 'Premium_LRS','Premium_ZRS','Standard_GRS','Standard_GZRS','Standard_LRS','Standard_RAGRS','Standard_RAGZRS','Standard_ZRS'."
   }
 }
