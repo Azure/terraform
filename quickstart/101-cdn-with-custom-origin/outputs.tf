27dba4b5787480745ad09d9c333876c88cb1cf06
Tom Archer <tarcher@microsoft.com>
2023-03-30 11:22:14 -0700

    Added CDN profile name to outputs

--- quickstart/101-cdn-with-custom-origin/outputs.tf
+++ quickstart/101-cdn-with-custom-origin/outputs.tf
@@ -2,10 +2,14 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+output "cdn_profile_name" {
+  value = azurerm_cdn_profile.profile.name
+}
+
 output "cdn_endpoint_endpoint_name" {
   value = azurerm_cdn_endpoint.endpoint.name
 }
 
-output "azurerm_cdn_endpoint_fqdn" {
+output "cdn_endpoint_fqdn" {
   value = azurerm_cdn_endpoint.endpoint.fqdn
 }
