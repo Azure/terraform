9cc0b625c6c9be79779d70e512167f29d5ed9ade
Tom Archer <tarcher@microsoft.com>
2023-04-02 09:04:42 -0700

    Edit per Code Review

--- quickstart/101-key-vault-key/variables.tf
+++ quickstart/101-key-vault-key/variables.tf
@@ -68,6 +68,6 @@ variable "key_size" {
 
 variable "msi_id" {
   type        = string
-  description = "The Managed Service Identity ID. If this value isn't null (the default), the Azure Key Vault Object ID will be set to this value."
+  description = "The Managed Service Identity ID. If this value isn't null (the default), 'data.azurerm_client_config.current.object_id' will be set to this value."
   default     = null
 }
