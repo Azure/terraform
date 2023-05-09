8f7335021a88fc26e6f8b22cb3de623b872446a9
Tom Archer <tarcher@microsoft.com>
2023-05-09 15:54:09 -0700

    User Story 89540

--- quickstart/101-azure-virtual-desktop/sig.tf
+++ quickstart/101-azure-virtual-desktop/sig.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "sigrg" {
 
 # generate a random string (consisting of four characters)
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-resource "random_string" "rando" {
+resource "random_string" "random" {
   length  = 4
   upper   = false
   special = false
