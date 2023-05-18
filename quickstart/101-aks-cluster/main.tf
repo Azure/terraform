7ae7a2616c1c274572b9902a9bb2aabc891f66a2
Tom Archer <tarcher@microsoft.com>
2023-05-18 08:34:54 -0700

    Added ssh.tf file

--- quickstart/101-aks-cluster/main.tf
+++ quickstart/101-aks-cluster/main.tf
@@ -7,27 +7,6 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
-resource "random_pet" "ssh_key_name" {
-  prefix    = "ssh"
-  separator = ""
-}
-
-resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
-  location  = "westus3"
-  parent_id = azurerm_resource_group.rg.id
-}
-
-resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
-
-  response_export_values = ["publicKey"]
-}
-
 resource "random_pet" "azurerm_kubernetes_cluster_name" {
   prefix = "cluster"
 }
@@ -40,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = random_pet.azurerm_kubernetes_cluster_name.id
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_dns_prefix.id
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
 
   identity {
     type = "SystemAssigned"
