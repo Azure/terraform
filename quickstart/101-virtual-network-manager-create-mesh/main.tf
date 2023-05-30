a543288e061e7db153d9c70bf4f58430a9cefbed
Michael Bender <michael.bender@microsoft.com>
2023-05-30 14:18:21 -0500

    Reviewer fixes

--- quickstart/101-virtual-network-manager-create-mesh/main.tf
+++ quickstart/101-virtual-network-manager-create-mesh/main.tf
@@ -15,60 +15,24 @@ resource "azurerm_resource_group" "rg" {
 resource "random_pet" "virtual_network_name" {
   prefix = "vnet"
 }
-resource "azurerm_virtual_network" "vnet_01" {
-  name                = "${random_pet.virtual_network_name.id}-01"
+resource "azurerm_virtual_network" "vnet" {
+  count = 3
+
+  name                = "${random_pet.virtual_network_name.id}-0${count.index}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/16"]
-
-  depends_on          = [azurerm_resource_group.rg]
-}
-
-resource "azurerm_virtual_network" "vnet_02" {
-  name                = "${random_pet.virtual_network_name.id}-02"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.1.0.0/16"]
-
-  depends_on          = [azurerm_resource_group.rg]
-}
-
-resource "azurerm_virtual_network" "vnet_03" {
-  name                = "${random_pet.virtual_network_name.id}-03"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.2.0.0/16"]
-
-  depends_on          = [azurerm_resource_group.rg]
+  address_space       = ["10.${count.index}.0.0/16"]
 }
 
 # Add a subnet to each virtual network
 
-resource "azurerm_subnet" "subnet_vnet_01" {
+resource "azurerm_subnet" "subnet_vnet" {
+  count = 3
+
   name                 = "default"
-  virtual_network_name = azurerm_virtual_network.vnet_01.name
+  virtual_network_name = azurerm_virtual_network.vnet[count.index].name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefixes     = ["10.0.0.0/24"]
-
-  depends_on           = [azurerm_virtual_network.vnet_01]
-}
-
-resource "azurerm_subnet" "subnet_vnet_02" {
-  name                 = "default"
-  virtual_network_name = azurerm_virtual_network.vnet_02.name
-  resource_group_name  = azurerm_resource_group.rg.name
-  address_prefixes     = ["10.1.0.0/24"]
-
-  depends_on           = [azurerm_virtual_network.vnet_02]
-}
-
-resource "azurerm_subnet" "subnet_vnet_03" {
-  name                 = "default"
-  virtual_network_name = azurerm_virtual_network.vnet_03.name
-  resource_group_name  = azurerm_resource_group.rg.name
-  address_prefixes     = ["10.2.0.0/24"]
-
-  depends_on           = [azurerm_virtual_network.vnet_03]
+  address_prefixes     = ["10.${count.index}.0.0/24"]
 }
 
 # Create a Virtual Network Manager instance
@@ -83,10 +47,8 @@ resource "azurerm_network_manager" "network_manager_instance" {
   scope_accesses      = ["Connectivity"]
   description         = "example network manager"
   scope {
-    subscription_ids  = [data.azurerm_subscription.current.id]
+    subscription_ids = [data.azurerm_subscription.current.id]
   }
-
-  depends_on          = [azurerm_resource_group.rg]
 }
 
 # Create a network group
@@ -94,25 +56,16 @@ resource "azurerm_network_manager" "network_manager_instance" {
 resource "azurerm_network_manager_network_group" "network_group" {
   name               = "network-group"
   network_manager_id = azurerm_network_manager.network_manager_instance.id
-
-  depends_on         = [azurerm_network_manager.network_manager_instance]
 }
 
-# Define network group membership
+# Add the three virtual networks to the network group as static members
 
-resource "azurerm_network_manager_static_member" "static_members_01" {
-  name                      = "static-member-01"
+resource "azurerm_network_manager_static_member" "static_members" {
+  count = 3
+
+  name                      = "static-member-0${count.index}"
   network_group_id          = azurerm_network_manager_network_group.network_group.id
-  target_virtual_network_id = azurerm_virtual_network.vnet_01.id
-
-  depends_on                = [azurerm_virtual_network.vnet_01]
-}
-resource "azurerm_network_manager_static_member" "static_members_02" {
-  name                      = "static-member-02"
-  network_group_id          = azurerm_network_manager_network_group.network_group.id
-  target_virtual_network_id = azurerm_virtual_network.vnet_02.id
-
-  depends_on                = [azurerm_virtual_network.vnet_02]
+  target_virtual_network_id = azurerm_virtual_network.vnet[count.index].id
 }
 
 # Create a connectivity configuration
@@ -125,8 +78,6 @@ resource "azurerm_network_manager_connectivity_configuration" "connectivity_conf
     group_connectivity = "None"
     network_group_id   = azurerm_network_manager_network_group.network_group.id
   }
-
-  depends_on           = [azurerm_network_manager_network_group.network_group]
 }
 
 
@@ -134,9 +85,7 @@ resource "azurerm_network_manager_connectivity_configuration" "connectivity_conf
 
 resource "azurerm_network_manager_deployment" "commit_deployment" {
   network_manager_id = azurerm_network_manager.network_manager_instance.id
-  location           = "eastus"
+  location           = azurerm_resource_group.rg.location
   scope_access       = "Connectivity"
   configuration_ids  = [azurerm_network_manager_connectivity_configuration.connectivity_config.id]
-
-  depends_on         = [azurerm_network_manager_connectivity_configuration.connectivity_config]
 }
