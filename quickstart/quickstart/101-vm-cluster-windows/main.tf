90164c2691eef8431871099dcccb2207a73a5ec7
Jingwei-MS <116267329+Jingwei-MS@users.noreply.github.com>
2023-05-31 14:58:14 +0800

    Update main.tf

--- quickstart/quickstart/101-vm-cluster-windows/main.tf
+++ quickstart/quickstart/101-vm-cluster-windows/main.tf
@@ -56,14 +56,6 @@ module "windowsservers" {
 
   depends_on = [azurerm_key_vault_access_policy.des]
 }
-  vnet_subnet_id      = module.network.vnet_subnets[0]
-  is_windows_image    = true
-  vm_hostname         = "vm-${random_string.windowsservers_vm_hostname.result}"
-  admin_password      = "ComplxP@ssw0rd!"
-  vm_os_simple        = "WindowsServer"
-  public_ip_dns       = [random_pet.windowsservers_public_ip_dns.id]
-  depends_on          = [azurerm_resource_group.rg]
-}
 
 module "network" {
   source              = "Azure/network/azurerm"
