9433e88bdf84431fdb16ced1173352baf5bbd690
Jingwei-MS <116267329+Jingwei-MS@users.noreply.github.com>
2023-05-31 14:56:35 +0800

    Update main.tf

--- quickstart/quickstart/101-vm-cluster-windows/main.tf
+++ quickstart/quickstart/101-vm-cluster-windows/main.tf
@@ -19,8 +19,43 @@ resource "random_pet" "windowsservers_public_ip_dns" {
 }
 
 module "windowsservers" {
-  source              = "Azure/compute/azurerm"
+  source              = "Azure/virtual_machine/azurerm"
+  image_os            = "windows"
   resource_group_name = azurerm_resource_group.rg.name
+  data_disks = [
+    for i in range(2) : {
+      name                 = "windowsdisk${random_id.id.hex}${i}"
+      storage_account_type = "Standard_LRS"
+      create_option        = "Empty"
+      disk_size_gb         = 1
+      attach_setting = {
+        lun     = i
+        caching = "ReadWrite"
+      }
+      disk_encryption_set_id = azurerm_disk_encryption_set.example.id
+    }
+  ]
+  new_boot_diagnostics_storage_account = {
+    customer_managed_key = {
+      key_vault_key_id          = azurerm_key_vault_key.storage_account_key.id
+      user_assigned_identity_id = azurerm_user_assigned_identity.storage_account_key_vault.id
+    }
+  }
+  network_interface_ids = azurerm_network_interface.windows_nic[*].id
+  new_network_interface = null
+  admin_username        = "azureuser"
+  admin_password        = random_password.win_password.result
+  name                  = "windows-${random_id.id.hex}"
+  os_disk = {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  vm_os_simple = "WindowsServer"
+  size         = var.size
+  subnet_id    = module.vnet.vnet_subnets[0]
+
+  depends_on = [azurerm_key_vault_access_policy.des]
+}
   vnet_subnet_id      = module.network.vnet_subnets[0]
   is_windows_image    = true
   vm_hostname         = "vm-${random_string.windowsservers_vm_hostname.result}"
