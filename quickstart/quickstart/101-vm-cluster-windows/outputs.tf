ffbe1fa1f3154e3ce47742862922347afb0c91fb
Jingwei-MS <116267329+Jingwei-MS@users.noreply.github.com>
2023-05-31 15:22:00 +0800

    Update outputs.tf

--- quickstart/quickstart/101-vm-cluster-windows/outputs.tf
+++ quickstart/quickstart/101-vm-cluster-windows/outputs.tf
@@ -1,1 +1,22 @@
+ value = azurerm_resource_group.rg.name
+}
 
+output "windows_vm_public_name" {
+  value = module.windowsservers.public_ip_dns_name
+}
+
+output "vm_public_ip" {
+  value = module.windowsservers.public_ip_address
+}
+
+output "vm_private_ips" {
+  value = module.windowsservers.network_interface_private_ip
+}
+
+output "vm_hostname" {
+  value = module.windowsservers.vm_names
+}
+  
+output "windows_vm_passpord" {
+  value     = random_passpord.win_password.result
+  sensitive = true
