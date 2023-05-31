2d7d554e05d8a91e4e11151b13f9f46f69611abc
Jingwei-MS <116267329+Jingwei-MS@users.noreply.github.com>
2023-05-31 15:25:32 +0800

    Update variables.tf

--- quickstart/quickstart/101-vm-cluster-windows/variables.tf
+++ quickstart/quickstart/101-vm-cluster-windows/variables.tf
@@ -1,1 +1,32 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location for all resources."
+}
 
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random value so name is unique in your Azure subscription."
+}
+
+variable "managed_identity_principal_id" {
+  type    = string
+  default = null
+}
+
+variable "my_public_ip" {
+  type    = string
+  default = null
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = null
+}
+
+variable "size" {
+  type     = string
+  default  = "Standard_F2"
+  nullable = false
+}
