5777e4a8ae3bc2a6727b44e07ab91775aa51470d
Tom Archer <tarcher@microsoft.com>
2023-06-06 14:40:25 -0700

    Added output var for randomly generated vnet names

--- quickstart/101-virtual-network-manager-create-mesh/outputs.tf
+++ quickstart/101-virtual-network-manager-create-mesh/outputs.tf
@@ -1,3 +1,7 @@
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+output "virtual_network_names" {
+  value = [for vnet in azurerm_virtual_network.vnet : vnet.name[*]]
+}
