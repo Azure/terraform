16950b836e8490f608802b58c97e53d2deb1af19
Tom Archer <tarcher@microsoft.com>
2023-07-21 07:38:57 -0700

    Changes to output SSH key (4)

--- quickstart/101-vm-cluster-linux/ssh.tf
+++ quickstart/101-vm-cluster-linux/ssh.tf
@@ -1,3 +1,8 @@
+resource "random_pet" "ssh_key_name" {
+  prefix    = "ssh"
+  separator = ""
+}
+
 resource "azapi_resource_action" "ssh_public_key_gen" {
   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   resource_id = azapi_resource.ssh_public_key.id
