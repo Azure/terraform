c82508aeaf8432b746434e80d27a36a11ff8175d
Tom Archer <tarcher@microsoft.com>
2023-08-10 13:17:53 -0700

    Small fix

--- quickstart/101-front-door-standard-premium/variables.tf
+++ quickstart/101-front-door-standard-premium/variables.tf
@@ -32,6 +32,6 @@ variable "front_door_sku_name" {
   default     = "Standard_AzureFrontDoor"
   validation {
     condition     = contains(["Standard_AzureFrontDoor", "Premium_AzureFrontDoor"], var.front_door_sku_name)
-    error_message = "The SKU value must be one of the following: Standard_AzureFrontDoor or Premium_AzureFrontDoor."
+    error_message = "The SKU value must be one of the following: Standard_AzureFrontDoor, Premium_AzureFrontDoor."
   }
 }
