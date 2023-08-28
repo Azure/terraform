39f24e4450cec05ecacbfd79d4e1cb446c004d53
cshea15 <chashea@microsoft.com>
2023-08-28 13:40:12 -0400

    changing the provider feature

--- quickstart/201-azfw-with-secure-hub/provider.tf
+++ quickstart/201-azfw-with-secure-hub/provider.tf
@@ -13,8 +13,9 @@ terraform {
 
 provider "azurerm" {
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false // Set to True for Production
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+      skip_shutdown_and_force_delete = true
     }
   }
 }
