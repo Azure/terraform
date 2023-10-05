708dbf9f80bfb5d175536129ddbc8c1c9e2aaeed
cshea15 <chashea@microsoft.com>
2023-10-05 17:00:51 -0400

    fixed nsg protocol error

--- quickstart/201-azfw-with-ipgroups/main.tf
+++ quickstart/201-azfw-with-ipgroups/main.tf
@@ -184,7 +184,7 @@ resource "azurerm_network_security_group" "vm_jump_nsg" {
     priority                   = 1000
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "SSH"
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
