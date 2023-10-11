a620142168f028a14cffe4ae9aaa307474789221
Ionut1115 <63416808+Ionut1115@users.noreply.github.com>
2023-10-11 17:33:36 +0200

    Update main.tf

--- quickstart/101-vm-with-infrastructure/main.tf
+++ quickstart/101-vm-with-infrastructure/main.tf
@@ -80,23 +80,6 @@ resource "random_id" "random_id" {
   byte_length = 8
 }
 
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_key_vault" "example" {
-  name                = "examplekv"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-
-  purge_protection_enabled = true
-}
-
 resource "azurerm_key_vault_access_policy" "client" {
   key_vault_id = azurerm_key_vault.example.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
@@ -118,24 +101,16 @@ resource "azurerm_key_vault_key" "example" {
   ]
 }
 
-
 resource "azurerm_storage_account" "ok_storage_account" {
   name                     = "examplestor"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
   identity {
     type = "SystemAssigned"
   }
-}
-
-
-resource "azurerm_storage_account_customer_managed_key" "ok_cmk" {
-  storage_account_id = azurerm_storage_account.ok_storage_account.id
-  key_vault_id       = azurerm_key_vault.example.id
-  key_name           = azurerm_key_vault_key.example.name
 }
 
 # Create virtual machine
