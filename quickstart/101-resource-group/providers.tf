c607e8db9e91d55199cee8d0c6f0f12a6e64eb8b
Tom Archer <tarcher@microsoft.com>
2023-10-10 18:34:10 -0700

    Updated sample (#256)

--- quickstart/101-resource-group/providers.tf
+++ quickstart/101-resource-group/providers.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">=0.12"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>3.0"
     }
     random = {
       source  = "hashicorp/random"
