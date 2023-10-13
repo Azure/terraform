6194b421cf2324b5e4c6fd0665cf355f2795291d
gruty <gruty@DESKTOP-L4JM3DK.lan>
2023-10-13 17:56:52 +0200

    Update main.tf to fix checkov alerts

--- quickstart/101-vm-with-infrastructure/main.tf
+++ quickstart/101-vm-with-infrastructure/main.tf
@@ -86,9 +86,11 @@ resource "azurerm_storage_account" "my_storage_account" {
   location                      = azurerm_resource_group.rg.location
   resource_group_name           = azurerm_resource_group.rg.name
   account_tier                  = "Standard"
-  account_replication_type      = "LRS"
+  account_replication_type      = "ZRS"
   min_tls_version               = "TLS1_2"
   public_network_access_enabled = false
+  allow_nested_items_to_be_public = false  
+
   queue_properties {
   	logging {
 		delete                  = true
