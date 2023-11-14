5766a155bb37d35d71cee0f5b72d1d3a402b6579
neil-yechenwei <yechenwei2007@hotmail.com>
2023-11-14 13:34:12 +0800

    update code

--- quickstart/201-vmss-jumpbox/main.tf
+++ quickstart/201-vmss-jumpbox/main.tf
@@ -10,7 +10,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "azurerm_resource_group" "vmss" {
