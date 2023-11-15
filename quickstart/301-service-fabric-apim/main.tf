06d7fb697408a84453643f7044109d8c57027e62
neil-yechenwei <yechenwei2007@hotmail.com>
2023-11-15 11:48:32 +0800

    update code

--- quickstart/301-service-fabric-apim/main.tf
+++ quickstart/301-service-fabric-apim/main.tf
@@ -2,8 +2,18 @@ data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
 
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = "=1.36.1"
   features {}
 }
 
