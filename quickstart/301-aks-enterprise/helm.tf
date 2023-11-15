33f69247bc3cf27a912ef5242161e1304e69a901
neil-yechenwei <yechenwei2007@hotmail.com>
2023-11-15 13:06:27 +0800

    update code

--- quickstart/301-aks-enterprise/helm.tf
+++ quickstart/301-aks-enterprise/helm.tf
@@ -1,14 +1,12 @@
 # Define the helm provider to use the AKS cluster
 provider "helm" {
   kubernetes {
-    host = "${azurerm_kubernetes_cluster.default.kube_config.0.host}"
+    host = azurerm_kubernetes_cluster.default.kube_config.0.host
 
-    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)}"
-    client_key             = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)}"
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
   }
-
-  service_account = "tiller"
 }
 
 # Install a load-balanced nginx-ingress controller onto the cluster
@@ -32,8 +30,8 @@ EOF
 }
 
 resource "helm_release" "ghost" {
-  name      = "ghost-blog"
-  chart     = "bitnami/ghost"
+  name  = "ghost-blog"
+  chart = "bitnami/ghost"
 
   depends_on = ["kubernetes_cluster_role_binding.tiller"]
 }
