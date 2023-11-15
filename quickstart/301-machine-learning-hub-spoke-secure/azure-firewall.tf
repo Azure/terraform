5df4e97fe48e7f0a665ded695e3f9e13f649a490
neil-yechenwei <yechenwei2007@hotmail.com>
2023-11-15 11:32:56 +0800

    update code

--- quickstart/301-machine-learning-hub-spoke-secure/azure-firewall.tf
+++ quickstart/301-machine-learning-hub-spoke-secure/azure-firewall.tf
@@ -48,7 +48,7 @@ resource "azurerm_firewall" "azure_firewall_instance" {
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.hub_rg.name
   firewall_policy_id  = azurerm_firewall_policy.base_policy.id
-  sku_name            = "AZFW_Hub"
+  sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
 
   ip_configuration {
