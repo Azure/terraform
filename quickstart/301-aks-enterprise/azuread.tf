470f2dd98d6449ba72ab3b5d49834eeb946f9f60
neil-yechenwei <yechenwei2007@hotmail.com>
2023-11-15 12:12:30 +0800

    update code

--- quickstart/301-aks-enterprise/azuread.tf
+++ quickstart/301-aks-enterprise/azuread.tf
@@ -1,9 +1,9 @@
 resource "azuread_application" "default" {
-  name = "${var.name}-${var.environment}"
+  display_name = "${var.name}-${var.environment}"
 }
 
 resource "azuread_service_principal" "default" {
-  application_id = "${azuread_application.default.application_id}"
+  application_id = azuread_application.default.application_id
 }
 
 resource "random_string" "password" {
@@ -12,13 +12,13 @@ resource "random_string" "password" {
 }
 
 resource "azuread_service_principal_password" "default" {
-  service_principal_id = "${azuread_service_principal.default.id}"
-  value                = "${random_string.password.result}"
+  service_principal_id = azuread_service_principal.default.id
+  value                = random_string.password.result
   end_date             = "2099-01-01T01:00:00Z"
 }
 
 resource "azurerm_role_assignment" "default" {
   scope                = "${data.azurerm_subscription.current.id}/resourceGroups/${azurerm_resource_group.default.name}"
   role_definition_name = "Network Contributor"
-  principal_id         = "${azuread_service_principal.default.id}"
+  principal_id         = azuread_service_principal.default.id
 }
--- quickstart/301-aks-enterprise/networking.tf
+++ quickstart/301-aks-enterprise/networking.tf
@@ -1,66 +1,66 @@
 # Virtual Network to deploy resources into
 resource "azurerm_virtual_network" "default" {
   name                = "${var.name}-vnet"
-  location            = "${azurerm_resource_group.default.location}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
   address_space       = ["${var.vnet_address_space}"]
 }
 
 # Subnets
 resource "azurerm_subnet" "aks" {
   name                 = "${var.name}-aks-subnet"
-  resource_group_name  = "${azurerm_resource_group.default.name}"
-  address_prefix       = "${var.vnet_aks_subnet_space}"
-  virtual_network_name = "${azurerm_virtual_network.default.name}"
+  resource_group_name  = azurerm_resource_group.default.name
+  address_prefixes     = [var.vnet_aks_subnet_space]
+  virtual_network_name = azurerm_virtual_network.default.name
 }
 
 resource "azurerm_subnet" "ingress" {
   name                 = "${var.name}-ingress-subnet"
-  resource_group_name  = "${azurerm_resource_group.default.name}"
-  virtual_network_name = "${azurerm_virtual_network.default.name}"
-  address_prefix       = "${var.vnet_ingress_subnet_space}"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = [var.vnet_ingress_subnet_space]
 }
 
 resource "azurerm_subnet" "gateway" {
   name                 = "${var.name}-gateway-subnet"
-  resource_group_name  = "${azurerm_resource_group.default.name}"
-  virtual_network_name = "${azurerm_virtual_network.default.name}"
-  address_prefix       = "${var.vnet_gateway_subnet_space}"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = [var.vnet_gateway_subnet_space]
 }
 
 # Network security groups
-resource azurerm_network_security_group "aks" {
+resource "azurerm_network_security_group" "aks" {
   name                = "${var.name}-aks-nsg"
-  location            = "${azurerm_resource_group.default.location}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
 }
 
-resource azurerm_network_security_group "ingress" {
+resource "azurerm_network_security_group" "ingress" {
   name                = "${var.name}-ingress-nsg"
-  location            = "${azurerm_resource_group.default.location}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
 }
 
-resource azurerm_network_security_group "gateway" {
+resource "azurerm_network_security_group" "gateway" {
   name                = "${var.name}-gateway-nsg"
-  location            = "${azurerm_resource_group.default.location}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
 }
 
 # Network security group associations
 resource "azurerm_subnet_network_security_group_association" "aks" {
-  subnet_id                 = "${azurerm_subnet.aks.id}"
-  network_security_group_id = "${azurerm_network_security_group.aks.id}"
+  subnet_id                 = azurerm_subnet.aks.id
+  network_security_group_id = azurerm_network_security_group.aks.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "ingress" {
-  subnet_id                 = "${azurerm_subnet.ingress.id}"
-  network_security_group_id = "${azurerm_network_security_group.ingress.id}"
+  subnet_id                 = azurerm_subnet.ingress.id
+  network_security_group_id = azurerm_network_security_group.ingress.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "gateway" {
-  subnet_id                 = "${azurerm_subnet.gateway.id}"
-  network_security_group_id = "${azurerm_network_security_group.gateway.id}"
+  subnet_id                 = azurerm_subnet.gateway.id
+  network_security_group_id = azurerm_network_security_group.gateway.id
 }
 
 
@@ -82,28 +82,28 @@ locals {
 }
 
 resource "azurerm_public_ip" "gateway" {
-  name                = "${local.gateway_ip_name}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
-  location            = "${azurerm_resource_group.default.location}"
-  domain_name_label   = "${local.gateway_name}"
+  name                = local.gateway_ip_name
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  domain_name_label   = local.gateway_name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_application_gateway" "gateway" {
-  name                = "${local.gateway_name}"
-  resource_group_name = "${azurerm_resource_group.default.name}"
-  location            = "${azurerm_resource_group.default.location}"
+  name                = local.gateway_name
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
 
   sku {
     name     = "WAF_v2"
     tier     = "WAF_v2"
-    capacity = "${var.gateway_instance_count}"
+    capacity = var.gateway_instance_count
   }
 
   gateway_ip_configuration {
-    name      = "${local.gateway_ip_config_name}"
-    subnet_id = "${azurerm_subnet.gateway.id}"
+    name      = local.gateway_ip_config_name
+    subnet_id = azurerm_subnet.gateway.id
   }
 
   frontend_port {
@@ -117,57 +117,57 @@ resource "azurerm_application_gateway" "gateway" {
   }
 
   frontend_ip_configuration {
-    name                 = "${local.frontend_ip_configuration_name}"
-    public_ip_address_id = "${azurerm_public_ip.gateway.id}"
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.gateway.id
   }
 
   backend_address_pool {
-    name         = "${local.backend_address_pool_name}"
+    name         = local.backend_address_pool_name
     ip_addresses = ["${var.ingress_load_balancer_ip}"]
   }
 
   backend_http_settings {
-    name                  = "${local.http_setting_name}"
+    name                  = local.http_setting_name
     cookie_based_affinity = "Disabled"
     port                  = 80
     protocol              = "http"
     request_timeout       = 1
-    probe_name            = "${local.probe_name}"
+    probe_name            = local.probe_name
   }
 
   http_listener {
     name                           = "${local.listener_name}-http"
-    frontend_ip_configuration_name = "${local.frontend_ip_configuration_name}"
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
     frontend_port_name             = "${local.frontend_port_name}-http"
     protocol                       = "http"
   }
 
   probe {
-    name                = "${local.probe_name}"
+    name                = local.probe_name
     protocol            = "http"
     path                = "/nginx-health"
     interval            = 30
     timeout             = 30
     unhealthy_threshold = 3
-    host                = "${var.ingress_load_balancer_ip}"
+    host                = var.ingress_load_balancer_ip
   }
 
   request_routing_rule {
-    name                           = "${local.request_routing_rule_name}-http"
-    rule_type                      = "PathBasedRouting"
-    http_listener_name             = "${local.listener_name}-http"
-    url_path_map_name              = "${local.url_path_map_name}"
+    name               = "${local.request_routing_rule_name}-http"
+    rule_type          = "PathBasedRouting"
+    http_listener_name = "${local.listener_name}-http"
+    url_path_map_name  = local.url_path_map_name
   }
 
   url_path_map {
-    name                               = "${local.url_path_map_name}"
-    default_backend_address_pool_name  = "${local.backend_address_pool_name}"
-    default_backend_http_settings_name = "${local.http_setting_name}"
-    
+    name                               = local.url_path_map_name
+    default_backend_address_pool_name  = local.backend_address_pool_name
+    default_backend_http_settings_name = local.http_setting_name
+
     path_rule {
-      name                       = "${local.url_path_map_rule_name}"
-      backend_address_pool_name  = "${local.backend_address_pool_name}"
-      backend_http_settings_name = "${local.http_setting_name}"
+      name                       = local.url_path_map_rule_name
+      backend_address_pool_name  = local.backend_address_pool_name
+      backend_http_settings_name = local.http_setting_name
       paths = [
         "/*"
       ]
