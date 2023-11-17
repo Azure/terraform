5895b4f1c47b43e075ef437a04d3c6484357b8ed
Michael Bender <michael.bender@microsoft.com>
2023-11-17 09:33:04 -0600

    updates from Review

--- quickstart/101-virtual-network-manager-create-management-group-scope/providers.tf
+++ quickstart/101-virtual-network-manager-create-management-group-scope/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.56.0"
+      version = "~> 3.56.0, < 4.0"
     }
     random = {
       source  = "hashicorp/random"
