4906b07493b1f58d2889c9b1a31312b38f6a58c8
neil-yechenwei <yechenwei2007@hotmail.com>
2023-12-27 09:17:40 +0800

    update code

--- quickstart/template/main.tf
+++ quickstart/template/main.tf
@@ -1,7 +1,7 @@
 # This file should include references to data sources, resources, and modules
 # required to manage the desired Azure infrastructure.
 
-resource "azurerm_resource_group" "my_rg" {
+resource "azurerm_resource_group" "myrg" {
   name     = var.var1
   location = "westeurope"
 }
