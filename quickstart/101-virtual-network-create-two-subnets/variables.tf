467bea358176372f6faba7f91fcb7cb3a030fe06
asudbring <allen@sudbring.com>
2024-01-18 22:46:17 -0800

    edited readme

--- quickstart/101-virtual-network-create-two-subnets/variables.tf
+++ quickstart/101-virtual-network-create-two-subnets/variables.tf
@@ -4,7 +4,7 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
-variable "prefix" {
+variable "resource_group_name_prefix" {
   type        = string
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
