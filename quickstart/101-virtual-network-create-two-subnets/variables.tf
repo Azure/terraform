ba8bdb816cafe35017a38671d628124cb8f9a91c
asudbring <allen@sudbring.com>
2024-01-18 23:06:22 -0800

    removed unused variable

--- quickstart/101-virtual-network-create-two-subnets/variables.tf
+++ quickstart/101-virtual-network-create-two-subnets/variables.tf
@@ -9,9 +9,3 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
-
-variable "vnet_address_space" {
-  type        = list(string)
-  default     = ["10.0.0.0/16"]
-  description = "Address space for the virtual network."
-}
