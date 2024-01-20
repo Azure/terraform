4bf711a541dd1a9001bd818de26e5aabf5d842ac
asudbring <allen@sudbring.com>
2024-01-19 23:58:31 -0800

    added sample for creating a NAT gateway with a virtual machine and corresponding network, subnet and public IP addresses

---
+++ quickstart/101-nat-gateway-create/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
