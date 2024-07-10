84022f9adfcba6538c870f484d04b521c4b81154
Tom Archer <tarcher@microsoft.com>
2024-07-10 08:56:57 -0700

    Fixing code

--- quickstart/101-aks-standard-lb-and-vmss/main.tf
+++ quickstart/101-aks-standard-lb-and-vmss/main.tf
@@ -1,4 +1,3 @@
-# Generate random resource group name
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
@@ -8,38 +7,35 @@ resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg_name.id
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_name" {
-  prefix = "cluster"
+resource "random_string" "aks_cluster_name" {
+  length  = 12
+  special = false
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
-  prefix = "dns"
-}
-
-resource "azurerm_kubernetes_cluster" "k8s" {
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = random_string.aks_cluster_name.result
   location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  dns_prefix = "myakscluster"
+
+  default_node_pool {
+    type       = "VirtualMachineScaleSets"
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+    enable_auto_scaling = true
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "Standard"
+  }
 
   identity {
     type = "SystemAssigned"
   }
 
-  default_node_pool {
-    name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
-    node_count = var.node_count
-  }
-  linux_profile {
-    admin_username = var.username
-
-    ssh_key {
-      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
-    }
-  }
-  network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
+  tags = {
+    Environment = "Production"
   }
 }
--- quickstart/101-aks-standard-lb-and-vmss/providers.tf
+++ quickstart/101-aks-standard-lb-and-vmss/providers.tf
@@ -1,11 +1,5 @@
 terraform {
-  required_version = ">=1.0"
-
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~>1.5"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
@@ -13,10 +7,6 @@ terraform {
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "0.9.1"
     }
   }
 }
