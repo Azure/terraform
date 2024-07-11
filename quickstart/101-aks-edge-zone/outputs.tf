9ae6b203a03da33973356724145892274a1a83f5
Tom Archer <tarcher@microsoft.com>
2024-07-11 06:45:14 -0700

    Working on edge zone name 2

--- quickstart/101-aks-edge-zone/outputs.tf
+++ quickstart/101-aks-edge-zone/outputs.tf
@@ -5,7 +5,3 @@ output "resource_group_name" {
 output "aks_cluster_name" {
   value = random_string.aks_cluster_name.result
 }
-
-output "edge_zone_name" {
-  value = random_string.edge_zone_name.result
-}
