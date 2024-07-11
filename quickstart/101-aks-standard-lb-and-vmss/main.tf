e9dfde8f8847b81bcad6b09185f4ee2ab3f34b4b
Tom Archer <tarcher@microsoft.com>
2024-07-11 09:23:27 -0700

    terraform fmt

--- quickstart/101-aks-standard-lb-and-vmss/main.tf
+++ quickstart/101-aks-standard-lb-and-vmss/main.tf
@@ -16,20 +16,20 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = random_string.aks_cluster_name.result
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix = "myakscluster"
+  dns_prefix          = "myakscluster"
 
   default_node_pool {
-    type       = "VirtualMachineScaleSets"
-    name       = "default"
-    node_count = 1
-    max_count = 3
-    min_count = 1
-    vm_size    = "Standard_DS2_v2"
+    type                = "VirtualMachineScaleSets"
+    name                = "default"
+    node_count          = 1
+    max_count           = 3
+    min_count           = 1
+    vm_size             = "Standard_DS2_v2"
     enable_auto_scaling = true
   }
 
   network_profile {
-    network_plugin = "azure"
+    network_plugin    = "azure"
     load_balancer_sku = "standard"
   }
 
