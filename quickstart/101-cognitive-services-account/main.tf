0a1eaef91386f26fdc95ac6022b3b8416a45bded
Eric Urban <eric.urban@microsoft.com>
2024-07-22 07:55:38 -0700

    Update main.tf
    I'm on the Azure AI services documentation team. According to PM, we recommend that customers start using the AIServices "kind" instead of the "CognitiveServices" kind. So, we want the sample to show how to create AIServices.
    
    This PR might take a dependency on one of these PRs to complete first.
    
    https://github.com/hashicorp/terraform-provider-azurerm/pull/26713
     
    https://github.com/hashicorp/terraform-provider-azurerm/pull/26008

--- quickstart/101-cognitive-services-account/main.tf
+++ quickstart/101-cognitive-services-account/main.tf
@@ -16,9 +16,9 @@ resource "random_string" "azurerm_cognitive_account_name" {
 }
 
 resource "azurerm_cognitive_account" "cognitive_service" {
-  name                = "CognitiveService-${random_string.azurerm_cognitive_account_name.result}"
+  name                = "AIServices-${random_string.azurerm_cognitive_account_name.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = var.sku
-  kind                = "CognitiveServices"
+  kind                = "AIServices"
 }
