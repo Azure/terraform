435555ac25a81e0bb6d3f196dd2e44dc38ceaa13
hezijie <lonegunmanb@hotmail.com>
2024-09-14 13:51:29 +0800

    set prevent_deletion_if_contains_resources to false

--- quickstart/201-vmss-packer-jumpbox/main.tf
+++ quickstart/201-vmss-packer-jumpbox/main.tf
@@ -27,7 +27,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "random_pet" "id" {}
