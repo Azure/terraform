286ef164dcb3194785f6892ffcee566820c0e4cb
hezijie <lonegunmanb@hotmail.com>
2024-09-14 13:17:14 +0800

    bump azurerm to v3 so we can use oidc

--- quickstart/201-vmss-packer-jumpbox/main.tf
+++ quickstart/201-vmss-packer-jumpbox/main.tf
@@ -5,10 +5,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>3.0"
     }
     azapi = {
-      source = "Azure/azapi"
+      source  = "Azure/azapi"
       version = "~> 1.0"
     }
     local = {
@@ -88,20 +88,18 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
 }
 
 resource "azurerm_lb_probe" "vmss" {
-  resource_group_name = azurerm_resource_group.vmss.name
-  loadbalancer_id     = azurerm_lb.vmss.id
-  name                = "ssh-running-probe"
-  port                = var.application_port
+  loadbalancer_id = azurerm_lb.vmss.id
+  name            = "ssh-running-probe"
+  port            = var.application_port
 }
 
 resource "azurerm_lb_rule" "lbnatrule" {
-  resource_group_name            = azurerm_resource_group.vmss.name
   loadbalancer_id                = azurerm_lb.vmss.id
   name                           = "http"
   protocol                       = "Tcp"
   frontend_port                  = var.application_port
   backend_port                   = var.application_port
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = azurerm_lb_probe.vmss.id
 }
