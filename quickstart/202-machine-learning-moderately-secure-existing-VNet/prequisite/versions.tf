69412cc3c0526d05231bde60e881241fb9c05cab
zjhe <lonegunmanb@hotmail.com>
2024-09-30 09:23:45 +0800

    bump azurerm provider version for prequisite

--- quickstart/202-machine-learning-moderately-secure-existing-VNet/prequisite/versions.tf
+++ quickstart/202-machine-learning-moderately-secure-existing-VNet/prequisite/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.78.0, <3.0"
+      version = "~> 3.0"
     }
     random = {
       source  = "hashicorp/random"
