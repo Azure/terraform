5b800a5809c14716ea1907299f56b7bf847a8016
Tom Archer <tarcher@microsoft.com>
2024-10-08 12:42:41 -0700

    Trying to fix IP address errors

--- quickstart/101-aks-extended-zones/main.tf
+++ quickstart/101-aks-extended-zones/main.tf
@@ -17,13 +17,13 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.virtual_network_name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["192.168.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   subnet {
     name           = "subnet1"
-    address_prefix = "10.1.1.0/24"
+    address_prefix = "192.168.1.0/24"
   }
 }
 
