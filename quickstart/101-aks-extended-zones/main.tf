c4da1e6fdd446555741eab017cc662f718a2897d
Tom Archer <tarcher@microsoft.com>
2024-10-08 12:00:04 -0700

    Initial put

---
+++ quickstart/101-aks-extended-zones/main.tf
@@ -0,0 +1,58 @@
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.1.1.0/24"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name           = "agentpool"
+    vm_size        = "Standard_D2_v2"
+    node_count     = var.aks_node_count
+    vnet_subnet_id = element(tolist(azurerm_virtual_network.vnet.subnet), 0).id
+  }
+
+  windows_profile {
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  edge_zone = var.aks_extended_zone
+}
