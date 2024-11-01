6b5c65949669682ed818f8b7478faa9181c6769a
Tom Archer <tarcher@microsoft.com>
2024-11-01 09:13:01 -0700

    cleaning up

--- quickstart/101-azure-functions/variables.tf
+++ quickstart/101-azure-functions/variables.tf
@@ -1,40 +1,53 @@
-variable "az_rg_name" {
-  type    = string
-  default = "azure-functions-example-rg"
+variable "resource_group_name" {
+  type        = string
+  default     = ""
+  description = "The name of the Azure resource group. If blank, a random name will be generated."
 }
 
-variable "location" {
-  type    = string
-  default = "East US"
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-variable "az_sa_account_tier" {
-  type    = string
-  default = "Standard"
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
 }
 
-variable "az_sa_account_replication_type" {
-  type    = string
-  default = "LRS"
+variable "sa_account_tier" {
+  description = "The tier of the storage account. Possible values are Standard and Premium."
+  type        = string
+  default     = "Standard"
 }
 
-variable "az_sa_name" {
-  type    = string
-  default = "examplefunctionssa"
+variable "sa_account_replication_type" {
+  description = "The replication type of the storage account. Possible values are LRS, GRS, RAGRS, and ZRS."
+  type        = string
+  default     = "LRS"
 }
 
-variable "az_asp_name" {
-  type    = string
-  default = "example-functions-service-plan"
+variable "sa_name" {
+  description = "The name of the storage account. If blank, a random name will be generated."
+  type        = string
+  default     = ""
 }
 
-variable "az_asp_sku_tier" {
-  type    = string
-  default = "Standard"
+variable "asp_name" {
+  description = "The name of the App Service Plan. If blank, a random name will be generated."
+  type        = string
+  default     = ""
 }
 
-variable "az_fa_name" {
-  type    = string
-  default = "example-functions-app"
+variable "asp_sku_tier" {
+  description = "The SKU tier of the App Service Plan. Possible values are Free, Shared, Basic, Standard, Premium, PremiumV2, and PremiumV3."
+  type        = string
+  default     = "Standard"
 }
 
+variable "fa_name" {
+  description = "The name of the Function App. If blank, a random name will be generated."
+  type        = string
+  default     = ""
+}
