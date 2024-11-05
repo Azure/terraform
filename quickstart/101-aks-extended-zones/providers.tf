e2f55bea37f084cdf7b941b56cbbc6d0bb7269d3
hezijie <lonegunmanb@hotmail.com>
2024-11-05 17:23:15 +0800

    try azapi

--- quickstart/101-aks-extended-zones/providers.tf
+++ quickstart/101-aks-extended-zones/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "2.0.1"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
