fdd218ca4cde62fd2a05e9bb9e469c1b24e7aece
Tom Archer <tarcher@microsoft.com>
2024-11-21 03:06:19 -0800

    Initial commit

---
+++ quickstart/101-azure-automation/main.tf
@@ -0,0 +1,23 @@
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+resource "random_string" "azurerm_automation_account_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
+
+resource "azurerm_automation_account" "example" {
+  name                = coalesce(var.automation_account_name, "autoacc-${random_string.azurerm_automation_account_name.result}")
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "Basic"
+}
