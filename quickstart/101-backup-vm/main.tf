db8048495746778db145e5cfb4927f0e87c019ab
Tom Archer <tarcher@microsoft.com>
2024-11-24 17:28:47 -0800

    removed iis server from win vm (2)

--- quickstart/101-backup-vm/main.tf
+++ quickstart/101-backup-vm/main.tf
@@ -127,6 +127,16 @@ resource "azurerm_windows_virtual_machine" "main" {
   }
 }
 
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
 resource "random_password" "password" {
   length      = 20
   min_lower   = 1
