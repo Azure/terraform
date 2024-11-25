2b86dd36da2cec355c20b1cdd59a8a03f7f6c813
Tom Archer <tarcher@microsoft.com>
2024-11-24 17:25:09 -0800

    removed iis server from win vm

--- quickstart/101-backup-vm/main.tf
+++ quickstart/101-backup-vm/main.tf
@@ -127,32 +127,6 @@ resource "azurerm_windows_virtual_machine" "main" {
   }
 }
 
-# Install IIS web server to the virtual machine
-resource "azurerm_virtual_machine_extension" "web_server_install" {
-  name                       = "${random_string.name.id}-wsi"
-  virtual_machine_id         = azurerm_windows_virtual_machine.main.id
-  publisher                  = "Microsoft.Compute"
-  type                       = "CustomScriptExtension"
-  type_handler_version       = "1.8"
-  auto_upgrade_minor_version = true
-
-  settings = <<SETTINGS
-    {
-      "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
-    }
-  SETTINGS
-}
-
-# Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
-}
-
 resource "random_password" "password" {
   length      = 20
   min_lower   = 1
