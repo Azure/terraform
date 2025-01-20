2b3ae3747ca394d059cf5551a0d5840cf977d6d7
hezijie <lonegunmanb@hotmail.com>
2025-01-20 12:20:26 +0800

    change default region to Canada Central to fix test

--- quickstart/101-cosmos-db-manualscale/variables.tf
+++ quickstart/101-cosmos-db-manualscale/variables.tf
@@ -6,13 +6,13 @@ variable "prefix" {
 
 variable "location" {
   type        = string
-  default     = "eastus"
+  default     = "Canada Central"
   description = "Resource group location"
 }
 
 variable "cosmosdb_account_location" {
   type        = string
-  default     = "eastus"
+  default     = "Canada Central"
   description = "Cosmos db account location"
 }
 
