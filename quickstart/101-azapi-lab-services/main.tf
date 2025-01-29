7b03d6fbd21e2f1d0d3707b4db7ba5cf1db2f4e3
Tom Archer <tarcher@microsoft.com>
2025-01-29 13:23:15 -0800

    Randomizing acr name

--- quickstart/101-azapi-lab-services/main.tf
+++ quickstart/101-azapi-lab-services/main.tf
@@ -3,7 +3,7 @@ resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
@@ -11,17 +11,25 @@ resource "azurerm_resource_group" "example" {
 # Create a user assigned identity resource.
 resource "azurerm_user_assigned_identity" "example" {
   name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+# Create a random name for the container registry.
+resource "random_string" "acr_name" {
+  length  = 10
+  special = false
+  upper   = false
+  numeric = false
 }
 
 # Manage a container registry resource.
 resource "azapi_resource" "example" {
   type      = "Microsoft.ContainerRegistry/registries@2020-11-01-preview"
-  name      = "registry1"
-  parent_id = azurerm_resource_group.example.id
+  name      = coalesce(var.container_registry_name, random_string.acr_name.result)
+  parent_id = azurerm_resource_group.rg.id
 
-  location = azurerm_resource_group.example.location
+  location = azurerm_resource_group.rg.location
   identity {
     type         = "SystemAssigned, UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.example.id]
