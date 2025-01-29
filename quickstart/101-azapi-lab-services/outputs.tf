4b9f8ad9c8e4d0d8621b09772eb140228e433344
Tom Archer <tarcher@microsoft.com>
2025-01-29 14:56:11 -0800

    Changed comment type in outputs.tf file

--- quickstart/101-azapi-lab-services/outputs.tf
+++ quickstart/101-azapi-lab-services/outputs.tf
@@ -1,9 +1,10 @@
-// Outputs "registry1.azurecr.io"
+
+# Outputs "registry1.azurecr.io"
 output "login_server" {
   value = azapi_resource.example.output.properties.loginServer
 }
 
-// Outputs  "disabled"
+# Outputs  "disabled"
 output "quarantine_policy" {
   value = azapi_resource.example.output.properties.policies.quarantinePolicy.status
 }
