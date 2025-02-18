55537b99ec0fdffeaf7f47bcd35aec7dde21e6d7
Tom Archer <tarcher@microsoft.com>
2025-02-18 15:52:36 -0800

    Fixed code 3

--- quickstart/101-virtual-network-public-ip-prefix/main.tf
+++ quickstart/101-virtual-network-public-ip-prefix/main.tf
@@ -14,8 +14,11 @@ resource "azurerm_public_ip_prefix" "my_ipv4" {
   name                = "myIPv4"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+
   ip_version    = "IPv4"
-  zones         = ["1", "2", "3"]
+  prefix_length = 28
+
+  zones = ["1", "2", "3"]
 }
 
 # Create a public IP prefix: IPv4 Zonal
@@ -25,7 +28,9 @@ resource "azurerm_public_ip_prefix" "my_ipv4_zonal" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_version    = "IPv4"
-  zones         = ["2"]
+  prefix_length = 28
+
+  zones = ["2"]
 }
 
 # Create a public IP prefix: IPv4 Non-Zonal
@@ -35,6 +40,7 @@ resource "azurerm_public_ip_prefix" "my_ipv4_non_zonal" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_version    = "IPv4"
+  prefix_length = 28
 }
 
 # Create a public IP prefix: IPv4 with Routing Preference set to Internet
@@ -43,19 +49,24 @@ resource "azurerm_public_ip_prefix" "my_ipv4_rp_internet" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
+  ip_version    = "IPv4"
+  prefix_length = 28
+
   tags = {
     RoutingPreference = "Internet"
   }
 }
 
-# Create a public IP prefix: IPv4 Zone redundant
+# Create a public IP prefix: IPv6 Zone redundant
 resource "azurerm_public_ip_prefix" "my_ipv6" {
   name                = "myIpv6"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_version    = "IPv6"
-  zones         = ["1", "2", "3"]
+  prefix_length = 124
+
+  zones = ["1", "2", "3"]
 }
 
 # Create a public IP prefix: IPv6 Zonal
@@ -65,7 +76,9 @@ resource "azurerm_public_ip_prefix" "my_ipv6_zonal" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_version    = "IPv6"
-  zones         = ["2"]
+  prefix_length = 124
+
+  zones = ["2"]
 }
 
 # Create a public IP prefix: IPv6 Non-Zonal
@@ -75,6 +88,7 @@ resource "azurerm_public_ip_prefix" "my_ipv6_non_zonal" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_version    = "IPv6"
+  prefix_length = 124
 }
 
 # Create a public IP (IPv4) and specify the public IP prefix
@@ -83,6 +97,7 @@ resource "azurerm_public_ip" "my_public_ip_ipv4" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
+
   ip_version          = "IPv4"
   public_ip_prefix_id = azurerm_public_ip_prefix.my_public_ip_prefix_ipv4.id
 }
@@ -92,7 +107,9 @@ resource "azurerm_public_ip_prefix" "my_public_ip_prefix_ipv4" {
   name                = "myPublicIpPrefix1"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  ip_version          = "IPv4" # Default
+
+  ip_version    = "IPv4" # Default
+  prefix_length = 28
 }
 
 # Create a public IP (IPv6) and specify the public IP prefix
@@ -101,6 +118,7 @@ resource "azurerm_public_ip" "my_public_ip_ipv6" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
+
   ip_version          = "IPv6"
   public_ip_prefix_id = azurerm_public_ip_prefix.my_public_ip_prefix_ipv6.id
 }
@@ -110,5 +128,7 @@ resource "azurerm_public_ip_prefix" "my_public_ip_prefix_ipv6" {
   name                = "myPublicIpPrefix2"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  ip_version          = "IPv6"
+
+  ip_version    = "IPv6"
+  prefix_length = 124
 }
