3e10a88752d3b110bd8a3615f94e2ae896e24735
hezijie <lonegunmanb@hotmail.com>
2025-03-07 11:33:34 +0800

    fix example

--- quickstart/101-aks-extended-zones/main.tf
+++ quickstart/101-aks-extended-zones/main.tf
@@ -15,20 +15,7 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
   prefix = "dns"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  address_space       = ["192.168.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "192.168.1.0/24"
-  }
-}
-
 resource "azapi_resource" "aks" {
-  count     = 1
   type      = "Microsoft.ContainerService/ManagedClusters@2024-05-01"
   name      = random_pet.azurerm_kubernetes_cluster_name.id
   parent_id = azurerm_resource_group.rg.id
@@ -38,7 +25,7 @@ resource "azapi_resource" "aks" {
   }
   body = {
     extendedLocation = {
-      name = var.aks_extended_zone
+      name = replace(lower(var.aks_extended_zone), " ", "")
       type = "EdgeZone"
     }
     sku = {
@@ -55,7 +42,6 @@ resource "azapi_resource" "aks" {
           vmSize                 = var.aks_node_vm_size
           osDiskSizeGB           = 128
           kubeletDiskType        = "OS"
-          vnetSubnetID           = element(tolist(azurerm_virtual_network.vnet.subnet), 0).id
           maxPods                = 30
           type                   = "VirtualMachineScaleSets"
           enableAutoScaling      = false,
