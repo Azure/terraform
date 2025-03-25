979ec964751b82425fc03caf9b121246d6d753ca
Tom Archer <tarcher@microsoft.com>
2025-03-25 11:49:44 -0700

    corrected name 2

--- quickstart/101-azure-storage-actions-create-storage-task/outputs.tf
+++ quickstart/101-azure-storage-actions-create-storage-task/outputs.tf
@@ -2,6 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "azapi_resource.storage_task_name" {
+output "storage_task_name" {
   value = azapi_resource.my_terraform_task.name
 }
