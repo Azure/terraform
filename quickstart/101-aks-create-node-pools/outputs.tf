bc35ca590576ec7d0bded0d53a49a9ca662b9e1c
Tom Archer <tarcher@microsoft.com>
2025-03-25 16:30:34 -0700

    initial put

---
+++ quickstart/101-aks-create-node-pools/outputs.tf
@@ -0,0 +1,27 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "kubernetes_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+}
+
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "kubernetes_cluster_location" {
+  value = azurerm_kubernetes_cluster.aks.location
+}
+
+output "kubernetes_cluster_dns_prefix" {
+  value = azurerm_kubernetes_cluster.aks.dns_prefix
+}
+
+output "kubernetes_cluster_node_count" {
+  value = azurerm_kubernetes_cluster.aks.default_node_pool[0].node_count
+}
+
+output "kubernetes_cluster_vm_size" {
+  value = azurerm_kubernetes_cluster.aks.default_node_pool[0].vm_size
+}
