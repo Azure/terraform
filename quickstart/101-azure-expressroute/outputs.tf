524f663741e9005fa80e9674c2d09082e3f7f4f6
duongau <28365324+duongau@users.noreply.github.com>
2025-09-23 14:04:22 -0700

    update to use managed public IP

--- quickstart/101-azure-expressroute/outputs.tf
+++ quickstart/101-azure-expressroute/outputs.tf
@@ -1,20 +1,50 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.rg.name
 }
 
 output "express_route_circuit_id" {
-  value = azurerm_express_route_circuit.circuit.id
+  description = "ID of the ExpressRoute circuit (if created)"
+  value       = var.create_express_route_circuit ? azurerm_express_route_circuit.circuit[0].id : null
+}
+
+output "express_route_circuit_service_key" {
+  description = "Service key for the ExpressRoute circuit (if created)"
+  value       = var.create_express_route_circuit ? azurerm_express_route_circuit.circuit[0].service_key : null
+  sensitive   = true
+}
+
+output "gateway_id" {
+  description = "ID of the ExpressRoute Virtual Network Gateway"
+  value       = module.expressroute_gateway.virtual_network_gateway.id
 }
 
 output "gateway_name" {
-  value = azurerm_virtual_network_gateway.gateway.name
+  description = "Name of the ExpressRoute Virtual Network Gateway"
+  value       = module.expressroute_gateway.virtual_network_gateway.name
 }
 
-output "gateway_ip" {
-  value = azurerm_public_ip.gateway_ip.ip_address
+output "gateway_subnet_id" {
+  description = "ID of the GatewaySubnet created by the module"
+  value       = module.expressroute_gateway.subnet.id
 }
 
-output "service_key" {
-  value     = azurerm_express_route_circuit.circuit.service_key
-  sensitive = true
+output "hosted_on_behalf_of_public_ip_note" {
+  description = "Information about the Azure-managed public IP for ExpressRoute gateway"
+  value       = "This ExpressRoute Virtual Network Gateway uses an Azure-managed public IP address. The public IP is automatically provisioned and managed by Azure, and is not visible in your subscription's public IP resources. This feature is only available for ExpressRoute gateways, not VPN gateways."
+}
+
+output "public_ip_addresses" {
+  description = "Public IP addresses created by the module (empty when using HOBO for ExpressRoute)"
+  value       = module.expressroute_gateway.public_ip_addresses
+}
+
+output "virtual_network_id" {
+  description = "ID of the virtual network"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "virtual_network_gateway_connections" {
+  description = "Virtual Network Gateway Connections created by the module"
+  value       = module.expressroute_gateway.virtual_network_gateway_connections
 }
--- quickstart/101-azure-expressroute/providers.tf
+++ quickstart/101-azure-expressroute/providers.tf
@@ -1,12 +1,18 @@
 terraform {
+  required_version = ">= 1.3"
+
   required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~>3.0"
+      version = "~> 3.5"
     }
   }
 }
