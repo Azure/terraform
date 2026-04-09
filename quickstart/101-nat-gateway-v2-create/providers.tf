5ea723aa65b9fbdf889959cba3d62c54d440ce1c
asudbring <allensu@microsoft.com>
2026-04-08 22:33:21 -0500

    docs: Add providers.tf for NAT Gateway V2 quickstart template

---
+++ quickstart/101-nat-gateway-v2-create/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
