e1e4295e035bccb10e482523530c635cf29765a5
Rebecca Calixte <v-rcalixte@microsoft.com>
2026-07-14 13:21:23 -0400

    Update AVD DSC module URL and endpoint setting

--- quickstart/101-azure-virtual-desktop/host.tf
+++ quickstart/101-azure-virtual-desktop/host.tf
@@ -108,11 +108,11 @@ resource "azurerm_virtual_machine_extension" "vmext_dsc" {
 
   settings = <<-SETTINGS
     {
-      "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_1.0.02714.342.zip",
+      "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_1.0.03419.1309.zip",
       "configurationFunction": "Configuration.ps1\\AddSessionHost",
       "properties": {
        "HostPoolName":"${azurerm_virtual_desktop_host_pool.hostpool.name}",
-       "UseAgentDownloadEndpoint": true
+       "UseAgentDownloadEndpoint": "true"
       }
     }
 SETTINGS
