97d8d51e1ae9c5d301cd52e60a0792c354dec3dd
RebeccaCalixte <262454636+Rebecca-Calixte@users.noreply.github.com>
2026-08-13 18:37:25 -0400

    Add AKS Cilium network policy quickstart

---
+++ quickstart/101-aks-network-policy-cilium/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-aks-network-policy-example"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "aks-network-policy-example"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "aksnetpolicyexample"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin      = "azure"
+    network_plugin_mode = "overlay"
+    network_policy      = "cilium"
+    network_data_plane  = "cilium"
+    load_balancer_sku   = "standard"
+  }
+}
