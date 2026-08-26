91d313c8c2b0afbfd4d49df78707b70d1fdf135f
Rebecca Calixte <v-rcalixte@microsoft.com>
2026-08-25 21:57:34 -0400

    Add Terraform configuration for AKS policy assignment

---
+++ quickstart/101-aks-use-azure-policy/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+variable "resource_group_name" {
+  description = "Name of the resource group that contains the existing AKS cluster."
+  type = string
+}
+variable "aks_cluster_name" {
+  description = "Name of the existing AKS cluster."
+  type = string
+}
+data "azurerm_resource_group" "aks" {
+  name = var.resource_group_name
+}
+data "azurerm_kubernetes_cluster" "aks" {
+  name = var.aks_cluster_name
+  resource_group_name = data.azurerm_resource_group.aks.name
+}
+data "azurerm_policy_set_definition" "aks_pod_security_baseline" {
+  display_name = "Kubernetes cluster pod security baseline standards for Linux-based workloads"
+}
+resource "azurerm_resource_group_policy_assignment" "aks_pod_security_baseline" {
+  name = "aks-pod-security-baseline"
+  display_name = "Kubernetes cluster pod security baseline standards for Linux-based workloads"
+  resource_group_id = data.azurerm_resource_group.aks.id
+  policy_definition_id = data.azurerm_policy_set_definition.aks_pod_security_baseline.id
+  parameters = jsonencode({
+    effect = {
+      value = "Deny"
+    }
+  })
+}
+ 
