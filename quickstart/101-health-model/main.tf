c714413bef74656e04fe4a50b8f96d90eea5b279
Bill Anderson <9596428+bandersmsft@users.noreply.github.com>
2026-09-01 14:23:00 -0700

    Clarify Health Models sample resources

--- quickstart/101-health-model/main.tf
+++ quickstart/101-health-model/main.tf
@@ -1,12 +1,15 @@
+# Create a random suffix for the resource names.
 resource "random_pet" "name" {
   length = 2
 }
 
+# Create a resource group for the health model.
 resource "azurerm_resource_group" "example" {
   name     = "${var.resource_group_name_prefix}-${random_pet.name.id}"
   location = var.resource_group_location
 }
 
+# Create a health model with a system-assigned managed identity.
 resource "azapi_resource" "health_model" {
   type      = "Microsoft.CloudHealth/healthmodels@2026-05-01-preview"
   name      = "${var.health_model_name_prefix}-${random_pet.name.id}"
